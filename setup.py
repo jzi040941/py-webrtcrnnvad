208e1ca5e7042234f35a1fe0d45526387bce40e5
Seonghun Noh <jzi040941@naver.com>
2022-09-01 01:52:06 +0000

    update setup

--- setup.py
+++ setup.py
@@ -121,13 +121,13 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="py_webrtcrnnvad",
     version="0.0.1",
-    author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
-    description="A test project using pybind11 and CMake",
+    author="Seonghun noh",
+    author_email="jzi004941@naver.com",
+    description="Python interface to the RNNoise VAD(Voice Activity Detection) component inside webrtc",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("py_webrtcrnnvad")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
